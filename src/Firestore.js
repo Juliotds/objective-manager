47430f6b19daedb417ef42f8c42fb5f60843d718
Julio TdS <takaguij@gmail.com>
2019-12-21 23:23:08 -0300

    informações do firebase nos enviroment variables

--- src/Firestore.js
+++ src/Firestore.js
@@ -1,13 +1,12 @@
 import firebase from "firebase";
-const config = {
-  apiKey: "AIzaSyAs1OJ-T67-rjr5GpV0fRG1-xscbk3ZuIk",
-  authDomain: "objective-manager.firebaseapp.com",
-  databaseURL: "https://objective-manager.firebaseio.com",
-  projectId: "objective-manager",
-  storageBucket: "objective-manager.appspot.com",
-  messagingSenderId: "831708805467",
-  appId: "1:831708805467:web:304cab4aa3a1010c5ec27e",
-  measurementId: "G-D4JFWGGB71"
+const devConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID
 };
-firebase.initializeApp(config);
+console.log(devConfig);
+firebase.initializeApp(devConfig);
 export default firebase;
